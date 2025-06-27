d28f5423e96e7c16c6731ff708a1a5080ed022bc
Owolabi <owolabi1738@gmail.com>
2025-06-27 20:23:29 +0100

    Created a reverse function that reverses a string

---
+++ reverse.js
@@ -0,0 +1,9 @@
+function reverse(word) {
+    let flipWord = [];
+    for (let i = word.length - 1; i >= 0; i--) {
+        flipWord.push(word[i]);
+    }
+    return flipWord.join('');
+}
+
+module exports = reverse;
