5c1052beb47b5f3880bebb5153fa643d0c7319af
Owolabi <owolabi1738@gmail.com>
2025-07-01 11:23:23 +0100

    Wrote two more lines of code to test he cipher function

--- cipher.test.js
+++ cipher.test.js
@@ -3,4 +3,5 @@ const ceasarCipher = require('./cipher');
     test('A function that shitfs each letter in a word by a specified number of positions in the alphabet', () => {
         expect(ceasarCipher('xyz', 3)).toBe('abc');
         expect(ceasarCipher('HeLLo', 3)).toBe('KhOOr');
+        expect(ceasarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
     });
